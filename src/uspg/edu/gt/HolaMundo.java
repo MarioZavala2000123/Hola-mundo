095288c1415cc01b957da325d0113a36a479d0b9
MarioZavala <mrzavalat@alumno.uspg.edu.gt>
2020-08-08 15:40:13 -0600

    mensaje hola como estas

--- src/uspg/edu/gt/HolaMundo.java
+++ src/uspg/edu/gt/HolaMundo.java
@@ -17,6 +17,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         
         System.out.println("Hola Mundo");
+        System.out.println("Hola que tal");
         
     }
     
