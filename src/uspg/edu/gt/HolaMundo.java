6c3a3b5988bb20440e7f68170d0d374ca623da11
MarioZavala <mrzavalat@alumno.uspg.edu.gt>
2020-08-15 15:26:09 -0600

    cambio en las ultimas lineas

--- src/uspg/edu/gt/HolaMundo.java
+++ src/uspg/edu/gt/HolaMundo.java
@@ -16,8 +16,7 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hola Mundo");
-        System.out.println("Hola que tal");
+        
         
     }
     
